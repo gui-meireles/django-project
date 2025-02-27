7eca62bd0afc16b0bb6664b95d1615e4f0a130d9
Guilherme Nascimento <guilherme.nascimento@extreme.digital>
2025-02-27 18:06:07 -0300

    feat: Personalizando tela de admin com filtro por categoria e quantidade de itens por pagina

--- galeria/admin.py
+++ galeria/admin.py
@@ -6,5 +6,7 @@ class ListandoFotografia(admin.ModelAdmin):
     list_display = ('id', 'nome', 'legenda')
     list_display_links = ('id', 'nome')
     search_fields = ('nome',)
+    list_filter = ('categoria',)
+    list_per_page = 15
 
 admin.site.register(Fotografia, ListandoFotografia)
